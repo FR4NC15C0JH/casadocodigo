39528fd2d4763d9740d24d55c0ce701ba159681e
FR4NC15C0JH <jh.programador@gmail.com>
2016-03-07 17:33:21 -0300

    Classe Book alterada

--- src/main/java/br/com/casadocodigo/loja/models/Book.java
+++ src/main/java/br/com/casadocodigo/loja/models/Book.java
@@ -2,6 +2,7 @@ package br.com.casadocodigo.loja.models;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -9,17 +10,37 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 public class Book {
 	
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	@NotBlank//verifica se a string é diferente de null e se também não é vazia
+	@NotNull
 	private String title;
+	@NotBlank
+	@NotNull
+	@Length(min=10)
 	private String description;
+	@Min(50)
 	private int numberOfPages;
+	@DecimalMin("20")//verifica se o ponto flutuante está entre o mínimo e o máximo
 	private BigDecimal price;
+	@NotNull
+	@Future
+	private Calendar releaseDate;
 	@ManyToMany
+	@Size(min=1)
+	@NotNull
 	private List<Author> authors = new ArrayList<>();
 	
 	public Integer getId() {
@@ -51,6 +72,12 @@ public class Book {
 	}
 	public void setPrice(BigDecimal price) {
 		this.price = price;
+	}
+	public Calendar getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Calendar releaseDate) {
+		this.releaseDate = releaseDate;
 	}
 	@Override
 	public String toString() {
